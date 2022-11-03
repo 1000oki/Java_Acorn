a1a20a203f298b8cc3418a7a92e817c4712c4caf
CheonOkHui <cjsdhrgml000@gmail.com>
2022-11-03 11:48:29 +0900

    step15 예제 추가

--- Step15_Thread/src/frame08/MyFrame.java
+++ Step15_Thread/src/frame08/MyFrame.java
@@ -47,25 +47,22 @@ public class MyFrame extends JFrame implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		System.out.println("카운트다운을 시작합니다.");
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				// 카운트 값을 저장할 지역번수 만들고 초기 값 대입
-				int count = 10;
-				while(true) {
-					System.out.println("현재 카운트: "+ count);
-					// 만일 count가 0이면
-					if(count == 0 ) {
-						// 반복문 탈출( 반복문 탈출하면 run() 메소드가 리턴되기 때문에 스레드가 종료됨.)
-						break;
-					}
-					 try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-					--count;
+		new Thread(()->{
+			// 카운트 값을 저장할 지역번수 만들고 초기 값 대입
+			int count = 10;
+			while(true) {
+				System.out.println("현재 카운트: "+ count);
+				// 만일 count가 0이면
+				if(count == 0 ) {
+					// 반복문 탈출( 반복문 탈출하면 run() 메소드가 리턴되기 때문에 스레드가 종료됨.)
+					break;
 				}
+				 try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e1) {
+					e1.printStackTrace();
+				}
+				--count;
 			}
 		}).start();
 	}
